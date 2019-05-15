5104b20e122963cfa50a21b658f7d8ffc3c87ef2
kondo <aa@kondo.local>
2019-05-15 01:20:27 -0700

    [Update] controller contacts_index_show users_index_show_edit_update_destroy

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,15 @@
 class ContactsController < ApplicationController
   def index
+  	@contacts = Contact.all
   end
 
   def show
+  	@contact = Contact.find(params[:id])
+  end
+
+
+  private
+  def contact_params
+  	params.require(:contact).permit(:user_id, :contact_content, :responce)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,40 @@
 class UsersController < ApplicationController
+
   def index
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+  	@user = User.find(params[:id])
+  	if @user.id != current_user.id || @user.id != current_user.admin
+  		redirect_to user_path(@user.id)
+  	end
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = 'You have updated user successfully.'
+      redirect_to user_path(@user.id)
+    else
+      render "/users/edit"
+    end
+  end
+
+  def destroy
+    @user = Book.find(params[:id])
+    @user.destroy
+    redirect_to '/users/index'
+  end
+
+
+　private
+  def user_params
+    params.require(:user).permit(:last_name, :first_name, :ruby_last_name, :ruby_first_name, :postcode, :address, :phone, :encrypted_password)
+  end
+
 end
