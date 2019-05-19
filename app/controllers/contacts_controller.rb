6b5551ba4a66cb77971caf4eb6db649baa5ea021
yoshihiro451 <y09076739471y@gmail.com>
2019-05-19 08:22:24 +0000

    views/add

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -15,10 +15,7 @@ class ContactsController < ApplicationController
 	end
 
 	def index
-<<<<<<< HEAD
-=======
 		@contact = Contact.new #あとで削除
->>>>>>> 956fc73964b25a71cc1ec659dda45c51f67518a5
 		@contacts = Contact.page(params[:page]).reverse_order
 	end
 
@@ -29,13 +26,10 @@ class ContactsController < ApplicationController
 	def update
 		@contact = Contact.find(params[:id])
 		@contact.user_id = current_user.admin
-<<<<<<< HEAD
-=======
 		if @contact.update(contact_params)
 			flash[:success] = '返信しました'
 			redirect_to "/contacts"
 		end
->>>>>>> 956fc73964b25a71cc1ec659dda45c51f67518a5
 	end
 
 	def destroy
@@ -47,10 +41,6 @@ class ContactsController < ApplicationController
 
 	private
 	def contact_params
-<<<<<<< HEAD
-		params.require(:contact).permit(:user_id, :contact_content, :responce)
-=======
 		params.require(:contact).permit(:user_id, :title, :contact_content, :responce)
->>>>>>> 956fc73964b25a71cc1ec659dda45c51f67518a5
 	end
 end
