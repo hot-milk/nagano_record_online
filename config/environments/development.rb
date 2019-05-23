ee5754062d2a648b6937e9889aead5cee578fbcc
kondo <aa@kondo.local>
2019-05-23 12:14:55 -0700

    [Update] Mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,12 +37,13 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    port: 587,
     address: 'smtp.gmail.com',
+    port: 587,
     domain: 'gmail.com',
     user_name: 'adtanaka.taro1111@gmail.com',
-    password: "tanakataro1111",
+    password: "nszgflcftvrxmjbs",
     authentication: 'plain',
     enable_starttls_auto: true
   }
